aa0f58a1c2a7e4c9c0bd3b108b90cf82e925b614
disaster_byte <11808043+disasterbyte@users.noreply.github.com>
2020-02-24 15:48:49 -0600

    init

---
+++ cssipher.py
@@ -0,0 +1,50 @@
+import sys
+import string
+import random
+
+if len(sys.argv) != 2:
+    exit('Usage: cssipher.py <input>')
+
+def generate_class():
+    return ''.join(random.choice(string.ascii_letters) for x in range(3))
+
+def generate_css():
+    css_output = ''
+    name_map = {}
+    for letter in string.ascii_letters:
+        if letter in input_string:
+            css_class = generate_class()
+            css_output += span_template.format(name=css_class, letter=letter)
+            name_map[letter] = css_class
+    return css_output, name_map
+
+span_template = '.{name} span {{ display: none;}} .{name}:after {{ content: "{letter}";}} '
+
+html_template = """
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <meta http-equiv="Content-Type" content="text/html charset=UTF-8"/>
+      <style type="text/css"> p {{ display: inline;}} {css}
+      </style>
+  </head>
+  <body>
+    {body}
+  </body>
+</html>
+"""
+
+input_string = str(sys.argv[1])
+output_string = ''
+css_output, letter_map = generate_css()
+
+for i in input_string:
+    if i in letter_map.keys():
+        output_string += '<p class="{}"><span>{}</span></p>'.format(letter_map[i], random.choice(string.ascii_letters))
+    else:
+        output_string += i
+
+html_output = html_template.format(css=css_output, body=output_string)
+
+with open('output.html', 'w') as f:
+    f.write(html_output)
